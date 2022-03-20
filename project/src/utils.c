71cc58fb5b7ca7b7844c4e925d92e6c7fbe27eb5
Eduard <Eduuaarrdd@yandex.ru>
2022-03-21 02:59:08 +0300

    отредактированы комментарии

--- project/src/utils.c
+++ project/src/utils.c
@@ -5,12 +5,12 @@ int timer_from(unsigned char from) {
     size_t counter = 0;
     for (unsigned char i = from; i != 0; --i) {
         ++counter;
-        printf("%c\n",i);
-    }
+        printf("%c", i);
+        }
     return counter;
 }
 
-// TODO: Implement `power of` function
+// TODO(Eduard): Implement `power of` function
 /*
 int custom_pow(int base, int power) {
     return 0;
